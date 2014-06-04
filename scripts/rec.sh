66e3788a7f4efc908be2ddc6745cb2817b17f487
Markus Skyttner <markus.skyttner@nrm.se>
2014-06-04 14:57:03 +0200

    fix rec.sh to use less cpu intensive RAW format

--- scripts/rec.sh
+++ scripts/rec.sh
@@ -1,3 +1,7 @@
+export AUDIODRIVER=alsa
 export AUDIODEV=hw:1,0
 export TIMESTAMP=`date +%Y%m%d_%H%M%S`
-nice -n -20 rec --buffer 32768  -c 1 -r 200k -b 16 -e signed "wav-$TIMESTAMP.wav" sinc 10k silence 1 0.001 1% 1 0:00:05 1%
+
+nice -n -20 rec --buffer 32768 -t RAW "raw/$TIMESTAMP.raw" sinc 10k silence 1 0.
+001 8% -1 0:00:05 8%
+sox -r 200k -b 16 -c 1 -e signed "raw/$TIMESTAMP.raw" "wav/$TIMESTAMP.wav"
